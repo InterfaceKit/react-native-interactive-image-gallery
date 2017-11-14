e2ec7defc9ab24122bd8b983d6b35905f9561bfb
alvaromb <amedina@apsl.net>
2017-11-14 12:00:44 +0100

    Right imports

--- ios/RNIKInteractiveImageLibrary.h
+++ ios/RNIKInteractiveImageLibrary.h
@@ -1,11 +1,6 @@
 
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#import "RCTEventEmitter.h"
-#else
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
-#endif
 #import <CoreMotion/CoreMotion.h>
 #import <QuartzCore/QuartzCore.h>
 
